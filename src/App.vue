28fb7c984ce24a2f9dae4f6fa6c65abc62580c5a
burakgur <burak@martiandigital.co>
2021-01-03 10:44:26 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,180 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>ToDo App</h1>
+	<form @submit.prevent="addTodo()">
+		<label>New ToDo </label>
+		<input
+			v-model="newTodo"
+			name="newTodo"
+			autocomplete="off"
+		>
+		<button>Add ToDo</button>
+	</form>
+	<h2>ToDo List</h2>
+	<ul>
+		<li
+			v-for="(todo, index) in todos"
+			:key="index"
+		>
+			<span
+				:class="{ done: todo.done }"
+				@click="doneTodo(todo)"
+			>{{ todo.content }}</span>
+			<button @click="removeTodo(index)">Remove</button>
+		</li>
+	</ul>
+	<h4 v-if="todos.length === 0">Empty list.</h4>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import { ref } from 'vue';
+	export default {
+		name: 'App',
+		setup () {
+			const newTodo = ref('');
+			const defaultData = [{
+				done: false,
+				content: 'Write a blog post'
+			}]
+			const todosData = JSON.parse(localStorage.getItem('todos')) || defaultData;
+			const todos = ref(todosData);
+			function addTodo () {
+				if (newTodo.value) {
+					todos.value.push({
+						done: false,
+						content: newTodo.value
+					});
+					newTodo.value = '';
+				}
+				saveData();
+			}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+			function doneTodo (todo) {
+				todo.done = !todo.done
+				saveData();
+			}
+
+			function removeTodo (index) {
+				todos.value.splice(index, 1);
+				saveData();
+			}
+
+			function saveData () {
+				const storageData = JSON.stringify(todos.value);
+				localStorage.setItem('todos', storageData);
+			}
+
+			return {
+				todos,
+				newTodo,
+				addTodo,
+				doneTodo,
+				removeTodo,
+				saveData
+			}
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+$border: 2px solid
+	rgba(
+		$color: white,
+		$alpha: 0.35,
+	);
+$size1: 6px;
+$size2: 12px;
+$size3: 18px;
+$size4: 24px;
+$size5: 48px;
+$backgroundColor: #27292d;
+$textColor: white;
+$primaryColor: #a0a4d9;
+$secondTextColor: #1f2023;
+body {
+	margin: 0;
+	padding: 0;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background-color: $backgroundColor;
+	color: $textColor;
+	#app {
+		max-width: 600px;
+		margin-left: auto;
+		margin-right: auto;
+		padding: 20px;
+		h1 {
+			font-weight: bold;
+			font-size: 28px;
+			text-align: center;
+		}
+		form {
+			display: flex;
+			flex-direction: column;
+			width: 100%;
+			label {
+				font-size: 14px;
+				font-weight: bold;
+			}
+			input,
+			button {
+				height: $size5;
+				box-shadow: none;
+				outline: none;
+				padding-left: $size2;
+				padding-right: $size2;
+				border-radius: $size1;
+				font-size: 18px;
+				margin-top: $size1;
+				margin-bottom: $size2;
+			}
+			input {
+				background-color: transparent;
+				border: $border;
+				color: inherit;
+			}
+		}
+		button {
+			cursor: pointer;
+			background-color: $primaryColor;
+			border: 1px solid $primaryColor;
+			color: $secondTextColor;
+			font-weight: bold;
+			outline: none;
+			border-radius: $size1;
+		}
+		h2 {
+			font-size: 22px;
+			border-bottom: $border;
+			padding-bottom: $size1;
+		}
+		ul {
+			padding: 10px;
+			li {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				border: $border;
+				padding: $size2 $size4;
+				border-radius: $size1;
+				margin-bottom: $size2;
+				span {
+					cursor: pointer;
+				}
+				.done {
+					text-decoration: line-through;
+				}
+				button {
+					font-size: $size2;
+					padding: $size1;
+				}
+			}
+		}
+		h4 {
+			text-align: center;
+			opacity: 0.5;
+			margin: 0;
+		}
+	}
 }
 </style>
